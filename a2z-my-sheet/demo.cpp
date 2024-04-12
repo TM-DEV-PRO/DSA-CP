ac80a9ed0631fa01e146fb48450adcd381ea8dcc
Tarun Mittal <tm.eng2021@gmail.com>
2024-04-12 16:02:33 +0530

    added compilers

---
+++ a2z-my-sheet/demo.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+const long long  INF = 1e18L + 5;
+const long double D_INF = 1e10;
+long double ans = 0.0;
+long long res = 0;
+long long int md = 1000000007LL;
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int x;
+cin >> x;
+cout << "hi" << x << endl;
+return 0;
+}
